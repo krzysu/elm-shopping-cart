97e3b6e967b12a5716ef2cc55d291d9c17f93738
Krzysztof Urbas <krzysu@interia.pl>
2017-10-22 20:21:31 +0200

    add styling

--- src/ShoppingCart.elm
+++ src/ShoppingCart.elm
@@ -51,35 +51,89 @@ update msg model =
         { model | cart = cart }
 
 -- VIEW
+css : String -> Html Msg
+css path =
+  node "link" [ rel "stylesheet", href path ] []
+
+priceWithCurrency : Float -> String
+priceWithCurrency price =
+  toString price ++ " EUR"
+
 view : Model -> Html Msg
 view model =
-  div [style [("display", "flex"), ("justifyContent", "space-around")]]
-    [ div [] [
-        h2 [] [text "Products"]
-        , div [] (List.map productView model.products)
-      ]
-    , div [] [
-        h2 [] [text "Shopping cart"]
-        , div [] (List.map cartView model.cart)
-        , cartTotalView model.cart
+  div [ class "container" ]
+    [ css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    , css "/public/stylesheets/index.css"
+
+    , div [ class "row" ]
+        [ div [ class "col-md-12" ]
+            [ h1 []
+                [ text "Elm Shopping Cart Example" ]
+            ]
+        ]
+    , div [ class "row" ]
+        [ div [ class "col-md-8" ]
+            [ h3 [] [text "Products"]
+            , div [ class "product-list" ] (List.map productItemView model.products)
+            ]
+        , div [ class "col-md-4" ]
+            [ h3 [] [text "Shopping cart"]
+            , cartView model.cart
+            ]
+        ]
+    , footer []
+        [ small []
+            [ text "made by "
+            , a [ href "https://twitter.com/krzysu" ]
+                [ text "Kris Urbas" ]
+            , text ", source code available on "
+            , a [ href "https://github.com/krzysu/elm-shopping-cart" ]
+                [ text "github" ]
+            ]
+        ]
+    ]
+
+productItemView : Product -> Html Msg
+productItemView product =
+  div [ class "product-list__item" ]
+    [ div [ class "product thumbnail" ]
+      [ img [ src "/public/images/01.jpg" ] []
+      , div [ class "caption" ]
+          [ h3 [] [text product.name]
+          , div [ class "product__price" ]
+              [ text (priceWithCurrency product.price) ]
+          , div [ class "product__button-wrap" ]
+              [ button
+                  [ class (if product.inCart then "btn btn-danger" else "btn btn-primary")
+                  , onClick (AddToCart product.id)
+                  ]
+                  [ text (if product.inCart then "Remove" else "Add") ]
+              ]
+          ]
       ]
     ]
 
-productView : Product -> Html Msg
-productView product =
-  div [style [("border", "1px solid grey"), ("margin", "0 0 10px")]]
-    [ div [] [text product.name]
-    , div [] [text ("price: " ++ toString product.price)]
-    , button
-        [onClick (AddToCart product.id)]
-        [text (if product.inCart then "Remove" else "Add")]
-    ]
+cartView : List Product -> Html Msg
+cartView products =
+  div [ class "cart" ]
+      [ div [ class "panel panel-default" ]
+          [ div [ class "panel-body" ]
+              [ div [] (List.map cartItemView products)
+              , if List.isEmpty products then
+                  div [ class "alert alert-info" ]
+                    [ text "Cart is empty" ] 
+                else
+                  text ""
+              , cartTotalView products
+              ]
+          ]
+      ]
 
-cartView : Product -> Html Msg
-cartView product =
-  div [style [("display", "flex"), ("justifyContent", "space-between")]]
-    [ div [] [text product.name]
-    , div [] [text ("price: " ++ toString product.price)]
+cartItemView : Product -> Html Msg
+cartItemView productInCart =
+  div [ class "cart-item"]
+    [ div [ class "cart-item__name"] [ text productInCart.name ]
+    , div [ class "cart-item__price" ] [ text (priceWithCurrency productInCart.price) ]
     ]
 
 cartTotalView : List Product -> Html Msg
@@ -87,4 +141,4 @@ cartTotalView cart =
   let
     total = List.foldr (+) 0 (List.map (\{ price } -> price) cart)
   in
-    div [] [text ("Total: " ++ toString total)]
+    div [ class "cart__total" ] [text ("Total: " ++ priceWithCurrency total)]
